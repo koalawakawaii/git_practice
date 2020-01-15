da5d30a18ee165d338995cb6e7ea1898ef93a300
satorus <kna.asa@gmail.com>
2020-01-15 14:26:11 +0900

    fizzbuzz完成

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,17 @@
 def fizzbuzz_conbert(number):
+    if number % 15 == 0:
+        return "fizzbuzz"
+
     if number % 3 == 0:
         return "fizz"
 
+    if number % 5 == 0:
+        return "buzz"
+
+    return str(number)
+
+
+for number in range(1, 101):
+    print(fizzbuzz_conbert(number))
 
 assert fizzbuzz_conbert(3) == "fizz"
